b80312255c6bb35bc689f2ad79b754f786e63108
Adam Harris <harris2010@gmail.com>
2014-12-14 23:32:06 -0800

    Updated with questionaire for user preferences.

--- Students/AdamHarris/session03/list_lab.py
+++ Students/AdamHarris/session03/list_lab.py
@@ -10,22 +10,17 @@ more_fruit = raw_input(u"Input another fruit: ")
 fruit.append(more_fruit)
 print(fruit)
 print(len(fruit))
+fruitindex = [] #Creating for error checking below
+
+for index in range(len(fruit)):
+    fruitindex.append(str(index))
+fruitindex.remove("0")
 
 #Number corresponding a fruit in the list.
 fruit_number= raw_input(u"Input a number and I'll return the corresponding fruit: ")
-try:
-    fruit_number=int(fruit_number)
-except:
-    ValueError or fruit_number<1 or fruit_number>len(fruit)
-    while ValueError or fruit_number<1 or fruit_number>len(fruit):
-        fruit_number = raw_input(u"Try again. Input a number and I'll return the corresponding fruit: ")
-        try:
-            fruit_number=int(fruit_number)
-            break
-        except:
-            print("Put in the correct inputs, homie.")
-if fruit_number<1 or fruit_number>len(fruit):
-    print("You've input a number out of range.") #There has to be a better way to error check for a number between 0-5.
+
+while fruit_number not in fruitindex:
+    fruit_number = raw_input(u"No such number in list. Input a number and I'll return the corresponding fruit: ")
 
 fruit_number = int(fruit_number)
 print(fruit[fruit_number-1])
@@ -36,7 +31,7 @@ fruit=second_fruit+fruit
 print(fruit)
 more_fruit = raw_input(u"Input a third fruit: ")
 fruit.insert(0,more_fruit)
-print(fruit)
+print(fruit, "Total list of fruit")
 
 
 pfruits=[]
@@ -58,7 +53,35 @@ while more_fruit not in pfruits:
 while more_fruit in pfruits:
     pfruits.remove(more_fruit)
 
-print(pfruits, "After Popcount removal")
+print(pfruits, "After fruit removal and doubled")
 
-userlike = raw_input(u"Do you like %s?" % pfruits[0])
-print(userlike)
+removallist=[] #list of fruits to remove after user completes the questionaire below.
+likelist=[] #list of fruits that remains after user identifies their preferences below.
+count = 0 #counter
+count1 = 0 #second counter
+pfruitscopy = pfruits[:] #Creating a copy in order to analyze list results of questionaire
+
+fruitstoquestion = [] #Creating a list of fruits for questionaire below.
+for count in range(len(pfruits)):
+    while pfruits[count] not in fruitstoquestion:
+        fruitstoquestion.append(pfruits[count])
+
+#Questionaire of user preference for fruits.
+print(fruitstoquestion, "Fruits to question")
+
+for count1 in range(len(fruitstoquestion)):
+    userpref = raw_input(u"Do you like %s? Please respond Yes or No. Trial" % fruitstoquestion[count1])
+    while userpref!="Yes" and userpref!="No":
+        userpref = raw_input(u"Do you like %s? Please respond Yes or No. No Me Gusta" % fruitstoquestion[count1])
+    if userpref == "No":
+        while fruitstoquestion[count1] in pfruitscopy:
+            pfruitscopy.remove(fruitstoquestion[count1])
+
+"""while userlike not in pfruits:
+    while pfruits[count] in removallist:
+        count+=1
+    while pfruits[count] in likelist:
+        count+=1"""
+
+print(pfruits, "Original list")
+print(pfruitscopy, "Updated list with user preferences")
