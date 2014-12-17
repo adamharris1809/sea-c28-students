53bae00ceeb16e203259fa6379652cdbf08d376f
Adam Harris <harris2010@gmail.com>
2014-12-16 22:51:14 -0800

    Cypher completed with error checking, minor bugs resolved.

--- Students/AdamHarris/session03/rot13.py
+++ Students/AdamHarris/session03/rot13.py
@@ -2,14 +2,12 @@
 
 """Creating a function that accepts any text each letter is replaced by the letter 13 characters away from it."""
 
-def rot13():
-    user_input = raw_input(u"Input text to scramble. ")
+def rot13(user_input = raw_input(u"Input text to scramble. ")):
     new_text = ""
 
     for letters in user_input:
         if letters.isalpha():
             alphabet = ord(letters)+13
-            print(ord(letters), ord(letters)+13)
             if alphabet > ord('z'):
                 alphabet -= 26
             if letters.isupper() and alphabet > ord('Z'):
@@ -17,5 +15,11 @@ def rot13():
             lastletter = chr(alphabet)
         new_text += lastletter
     print(new_text)
+    return(new_text)
 
 rot13()
+
+if __name__ == "__main__":
+    """Checking to see if the function works properly."""
+    assert rot13("abcdefghijklmnopqrstuvwxyz") == "nopqrstuvwxyzabcdefghijklm"
+    assert rot13("ABCDEFGHIJKLMNOPQRSTUVWXYZ") == "NOPQRSTUVWXYZABCDEFGHIJKLM"
