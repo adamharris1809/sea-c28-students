677bb0722ee18e3e5cea35ac210e3913fba27686
Adam Harris <harris2010@gmail.com>
2015-01-04 14:24:25 -0800

    Second iteration.

--- Students/AdamHarris/session05/clean_file.py
+++ Students/AdamHarris/session05/clean_file.py
@@ -1,16 +1,19 @@
 import io
+import sys
 
-def clean_file(fileinput, fileoutput):
-    f_input = io.open('').read()
-    #inlines = io.open(f_input).readlines() - test the difference.
-    f_output = f_input.strip()
+def clean_file(name_fileinput, name_fileoutput):
+    f_inlines = io.open(name_fileinput).read()
+    #inlines = io.open(name_fileinput).readlines() - test the difference.
+    f_outlines = f_input.strip()
+    f_outfile = io.open(name_fileoutput, 'w')
+    for line in f_outlines:
+        f_outfile.write(line"\n")
 
 
-    f_output = io.open(fileoutput, 'w')
-    for line in f_output:
-        f_output.write(line "\n")
+if __name__ == "__main__":
+    filename = sys.argv[1]
 
-
+"""
 #from class example
 outfile = io.open('output.txt', 'w')
 for i in range(10):
@@ -18,3 +21,5 @@ for i in range(10):
 
 #from solutions manual
 f_output = map(string.strip, f_input)
+
+"""
