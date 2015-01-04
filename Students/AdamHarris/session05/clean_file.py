4d33c82ff9cb60799c49b15d30d889af88e98127
Adam Harris <harris2010@gmail.com>
2015-01-04 15:39:19 -0800

    Bugs with validation section. Submit and ask questions.

--- Students/AdamHarris/session05/clean_file.py
+++ Students/AdamHarris/session05/clean_file.py
@@ -1,9 +1,10 @@
 import io
 import sys
+import string
 
 def clean_file(name_fileinput, name_fileoutput):
     f_inlines = io.open(name_fileinput).readlines()
-    f_outlines = map(string.strip, f_input)
+    f_outlines = map(string.strip, f_inlines)
     f_outfile = io.open(name_fileoutput, 'w')
     for line in f_outlines:
         f_outfile.write(line+"\n")
@@ -13,17 +14,17 @@ def clean_file_LC(name_fileinput, name_fileoutput):
 
     f_outlines = [line.strip()+"\n" for line in f_inlines]
 
-    f_outfile = io.open(name_fileoutput, 'w'), writelines(f_outlines)
+    f_outfile = io.open(name_fileoutput, 'w').writelines(f_outlines)
 
 
 
 """I don't like the idea of taking credit for leaning entirely on the solutions manual
 to understand/write this name = main section below.
 """
-
+"""
 if __name__ == "__main__":
     if len(sys.argv) < 2:
-        print help_msg
+        print("Seek help menu, try again.")
         sys.exit(1)
     else:
         infilename = sys.argv[1]
@@ -31,8 +32,8 @@ if __name__ == "__main__":
         outfilename = sys.argv[2]
     except IndexError:
         outfilename = infilename
+"""
 
-clean_file('home/adamharris/sea-c28-stuff/sea-c28-students/Students/AdamHarris/session04/sherlock.txt',
-'home/adamharris/sea-c28-stuff/sea-c28-students/Students/AdamHarris/session04/sherlock1.txt')
-#clean_fileLC(name_fileinput, name_fileoutput)
+clean_file_LC('/home/adamharris/sea-c28-stuff/sea-c28-students/Students/AdamHarris/session04/sherlock.txt',
+'/home/adamharris/sea-c28-stuff/sea-c28-students/Students/AdamHarris/session04/sherlock1.txt')
 
