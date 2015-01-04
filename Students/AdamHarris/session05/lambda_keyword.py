54d3ea26e52ac4300a2efee06e65074018b4a63e
Adam Harris <harris2010@gmail.com>
2015-01-04 12:32:37 -0800

    Updated & Final for lambda_keyword.

--- Students/AdamHarris/session05/lambda_keyword.py
+++ Students/AdamHarris/session05/lambda_keyword.py
@@ -3,6 +3,10 @@
 """Create a function that returns a list of n functions that when called will //
 return input value by an increasing number"""
 
+""" Original attempt added here in comments. The y=y makes sense; didn't recognize the necessity right away.
 def function_builder(num):
-    print([lambda x,y: x+y for y in range(num)])
+    return([lambda x,y: x+y for y in range(num)])
+"""
 
+def function_builder(num):
+    return([lambda x, y=y: x+y for y in range(num)])
