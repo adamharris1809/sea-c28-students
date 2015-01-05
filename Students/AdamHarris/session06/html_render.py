e4fc8d2acf74c7b4513bec7c72211343102c8443
Adam Harris <harris2010@gmail.com>
2015-01-05 11:45:52 -0800

    Progress at end of Step 2.

--- Students/AdamHarris/session06/html_render.py
+++ Students/AdamHarris/session06/html_render.py
@@ -4,17 +4,22 @@ class Element(object):
     """Create an html object."""
     opening_tag = "<>"
     closing_tag = "</>"
-    indent = ""
-
-class ind(Element):
+    tag = u"html"
     indent = "    "
 
     def __init__(self, content = ""):
 
-        def append(add_content):
-            content.append(add_content)
+    def append(add_content):
+        content.append(add_content)
 
-        def render(self, file_out, ind = ""):
-            all_out = [self.opening_tag] + self + append + self.closing_tag
-            print "\n".join(all_out)
-            file_out.write(all_out)
+    def render(self, file_out, ind = ""):
+        all_out = [self.opening_tag] + self + append.content + self.closing_tag
+        print "\n".join(all_out)
+        file_out.write(all_out)
+
+class body(Element):
+    tag = "<body>"
+
+class p(Element):
+    tag = u'<p>'
+
